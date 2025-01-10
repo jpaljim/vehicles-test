723bd2d0d600f7f533ba39bc454232206c6ac02c
Javier Palomo Jiménez <javier.palomo@fundaciontrilema.org>
2025-01-10 01:05:52 +0100

    Add HeaderComponent to app layout and style adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '@shared/components/index';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
