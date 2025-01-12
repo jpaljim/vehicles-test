cbf6ed817d447cdc684e46acd959e0a0689af5ee
Javier Palomo Jiménez <javier.palomo@fundaciontrilema.org>
2025-01-12 22:28:00 +0100

    Implement virtual scrolling for make list and enhance search functionality

--- src/app/components/makes/make-list/make-list.component.ts
+++ src/app/components/makes/make-list/make-list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { Make } from '@models/make';
 
 @Component({
   selector: 'app-make-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatListModule,
+    ScrollingModule,
+  ],
   templateUrl: './make-list.component.html',
   styleUrl: './make-list.component.scss'
 })
 export class MakeListComponent {
+
+  @Input({ required: true }) makes: Make[] = [];
+
+  @Output() clickEvent = new EventEmitter<Make>();
+
+  click(make: Make): void {
+    this.clickEvent.emit(make);
+  }
 
 }
--- src/app/pages/makes/make-list/make-list.component.ts
+++ src/app/pages/makes/make-list/make-list.component.ts
@@ -1,26 +1,61 @@
-import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
 import { MakeListComponent } from '@components/index';
+import { FilterPipe } from '@pipes/filter.pipe';
 import { VehicleMakesStore } from '@stores/vehicle-makes.store';
+import { debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-make-list-page',
   standalone: true,
   imports: [
-    JsonPipe,
+    FilterPipe,
     MakeListComponent,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './make-list.component.html',
   styleUrl: './make-list.component.scss',
-  providers: [VehicleMakesStore],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MakeListPageComponent implements OnInit {
+export class MakeListPageComponent implements OnInit, OnDestroy {
 
   readonly vehicleMakesStore = inject(VehicleMakesStore);
+  form: FormGroup;
+  query = signal('');
+
+  private _formBuilder = inject(FormBuilder);
+  private _router = inject(Router);
+  private _querySub?: Subscription;
+
+  constructor() {
+    this.form = this._formBuilder.group({
+      query: new FormControl(''),
+    });
+  }
 
   ngOnInit(): void {
     this.vehicleMakesStore.loadMakes();
+    this._querySub = this.form.get('query')?.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+    ).subscribe((query) => this.query.set(query))
+  }
+
+  ngOnDestroy(): void {
+    if (this._querySub) this._querySub.unsubscribe();
+  }
+
+  navigateToMakeDetails(makeId: number): void {
+    this._router.navigate(['/makes', makeId]);
   }
 
 }
