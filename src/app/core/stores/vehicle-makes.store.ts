07ea97deb60aec614502aaaa34d9de2a328be585
Javier Palomo Jiménez <javier.palomo@fundaciontrilema.org>
2025-01-12 23:56:41 +0100

    Refactor VehicleMakesStore: add initialLoad state to prevent redundant data loading

--- src/app/core/stores/vehicle-makes.store.ts
+++ src/app/core/stores/vehicle-makes.store.ts
@@ -4,28 +4,37 @@ import { tapResponse } from '@ngrx/operators';
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { VehiclesService } from '@services/vehicles.service';
-import { pipe, switchMap, tap } from 'rxjs';
+import { of, pipe, switchMap, tap } from 'rxjs';
 
 interface VehicleMakesState {
+  initialLoad: boolean;
   isLoading: boolean;
   makes: Make[];
 };
 
 const initialState: VehicleMakesState = {
+  initialLoad: false,
   isLoading: false,
   makes: [],
 };
 
 export const VehicleMakesStore = signalStore(
+  { providedIn: 'root' },
   withState(initialState),
   withMethods((store, vehiclesService = inject(VehiclesService)) => ({
     loadMakes: rxMethod<void>(
       pipe(
-        tap(() => patchState(store, { isLoading: true })),
+        tap(() => {
+          if (store.initialLoad()) return;
+          patchState(store, { isLoading: true });
+        }),
         switchMap(() => {
+          if (store.initialLoad()) {
+            return of(store.makes);
+          }
           return vehiclesService.getAllMakes().pipe(
             tapResponse({
-              next: (response) => patchState(store, { makes: response.Results, isLoading: false }),
+              next: (response) => patchState(store, { makes: response.Results, isLoading: false, initialLoad: true }),
               error: (err) => {
                 patchState(store, { isLoading: false });
                 console.error(err);
