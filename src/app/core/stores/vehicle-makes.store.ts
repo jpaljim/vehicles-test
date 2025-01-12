bc03ac253cca3d3519b34c59b7351792bfbb4027
Javier Palomo Jiménez <javier.palomo@fundaciontrilema.org>
2025-01-12 22:04:11 +0100

    Refactor VehicleMakesStore: remove filter state and update loadMakes method to handle response structure

--- src/app/core/stores/vehicle-makes.store.ts
+++ src/app/core/stores/vehicle-makes.store.ts
@@ -1,19 +1,17 @@
 import { inject } from '@angular/core';
 import { Make } from '@models/make';
+import { tapResponse } from '@ngrx/operators';
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { VehiclesService } from '@services/vehicles.service';
 import { pipe, switchMap, tap } from 'rxjs';
-import { tapResponse } from '@ngrx/operators';
 
 interface VehicleMakesState {
-  filter: { query: string; };
   isLoading: boolean;
   makes: Make[];
 };
 
 const initialState: VehicleMakesState = {
-  filter: { query: '' },
   isLoading: false,
   makes: [],
 };
@@ -21,22 +19,13 @@ const initialState: VehicleMakesState = {
 export const VehicleMakesStore = signalStore(
   withState(initialState),
   withMethods((store, vehiclesService = inject(VehiclesService)) => ({
-    updateQuery(query: string): void {
-      patchState(store, (state) => (
-        {
-          filter: {
-            ...state.filter, query
-          },
-        }
-      ));
-    },
     loadMakes: rxMethod<void>(
       pipe(
         tap(() => patchState(store, { isLoading: true })),
         switchMap(() => {
           return vehiclesService.getAllMakes().pipe(
             tapResponse({
-              next: (makes) => patchState(store, { makes, isLoading: false }),
+              next: (response) => patchState(store, { makes: response.Results, isLoading: false }),
               error: (err) => {
                 patchState(store, { isLoading: false });
                 console.error(err);
