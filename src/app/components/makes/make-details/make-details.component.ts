1d9c826d1b4d03db4c43e93f320e5fb3d97dac97
Javier Palomo Jiménez <javier.palomo@fundaciontrilema.org>
2025-01-13 02:22:20 +0100

    Refactor make-details component to include MakeTypeList and MakeModelList; enhance styling and navigation functionality

--- src/app/components/makes/make-details/make-details.component.ts
+++ src/app/components/makes/make-details/make-details.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MakeModel } from '@models/make-model';
+import { MakeType } from '@models/make-type';
+import { MakeModelListComponent } from '../make-model-list/make-model-list.component';
+import { MakeTypeListComponent } from '../make-type-list/make-type-list.component';
 
 @Component({
   selector: 'app-make-details',
   standalone: true,
-  imports: [],
+  imports: [
+    MakeModelListComponent,
+    MakeTypeListComponent,
+  ],
   templateUrl: './make-details.component.html',
   styleUrl: './make-details.component.scss'
 })
 export class MakeDetailsComponent {
+
+  @Input({ required: true }) makeTypes: MakeType[] = [];
+  @Input({ required: true }) makeModels: MakeModel[] = [];
 
 }
--- src/app/pages/makes/make-details/make-details.component.ts
+++ src/app/pages/makes/make-details/make-details.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VehicleModelsStore } from '@stores/vehicle-models.store';
+import { VehicleTypesStore } from '@stores/vehicle-types.store';
+import { MakeDetailsComponent } from "../../../components/makes/make-details/make-details.component";
 
 @Component({
   selector: 'app-make-details-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MakeDetailsComponent,
+  ],
   templateUrl: './make-details.component.html',
-  styleUrl: './make-details.component.scss'
+  styleUrl: './make-details.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MakeDetailsPageComponent {
+export class MakeDetailsPageComponent implements OnInit {
+
+  vehicleTypesStore = inject(VehicleTypesStore);
+  vehicleModelsStore = inject(VehicleModelsStore);
+
+  private _activatedRoute = inject(ActivatedRoute);
+  private _router = inject(Router);
+
+  ngOnInit(): void {
+    const id = this._activatedRoute.snapshot.paramMap.get('slug');
+
+    if (!id) return;
+    this.vehicleTypesStore.loadTypes(+id);
+    this.vehicleModelsStore.loadModels(+id);
+  }
+
+  navigateBackToList() {
+    this._router.navigate(['/makes']);
+  }
 
 }
